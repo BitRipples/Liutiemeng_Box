087ab663e39e8b80bb17e677845cb03d659645ba
凯 何 <1322571433@qq.com>
2021-11-09 19:49:42 +0800

    添加项目文件。

---
+++ 11-8-【1】/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_8__1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*////事件Event
+            //Person hekai = new Person();
+            ////属性，方法，事件*/
+            ///
+            /// 属性方法 事件重要
+            ///
+            Person hekai = new Person();
+            /*hekai.Shuanshu(1, 100);*/
+            hekai.diguisuanshu(1, 100, 0);
+        }
+    }
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public void Sayhello()
+        {
+            Console.WriteLine("my name is {0},i'm {1} years old.",Name,Age);
+        }
+        /*public void Shuanshu(int x, int y)
+        {
+            int result = 0;
+            for (int i = x; i < y; i++)
+            {
+                result += i;
+            }
+            result += y;
+            Console.WriteLine(result);
+        }*/
+
+        public void diguisuanshu(int x,int y,int result)
+        {
+            int Result=result;          
+            if(x<y)
+            {
+                Result += x;
+                x++;
+                diguisuanshu(x, y,Result);
+            }
+            else
+            {
+                Result += y;
+                Console.WriteLine(Result);
+            }
+        }
+    }
+}
---
+++ 21-11-4/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _21_11_4
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("你点击了一个按钮");
+        }
+    }
+}
